03b2a172a5044f61cacde80f7481afed0bdc5c8c
Aditya <adityaprasadmohanty150@gmail.com>
2022-02-27 12:44:28 +0530

    Modified

---
+++ components/Home/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const header = () => {
+  return (
+    <View>
+      <Text style={{color: 'white'}}>Header</Text>
+    </View>
+  );
+}
+
+export default header
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { SafeAreaView, View, Text } from 'react-native'
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native'
+import Header from '../components/Home/Header'
 
 const HomeScreen = () => {
   return (
-      <SafeAreaView>
-          <Text>
-              HomeScreen
-          </Text>
+      <SafeAreaView style={styles.container}>
+          <Header />
       </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black',
+        flex: 1,
+    },
+})
 export default HomeScreen
