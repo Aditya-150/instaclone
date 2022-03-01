3b12c2f813e070ffd73e99db7f1c9d2051202988
Aditya <adityaprasadmohanty150@gmail.com>
2022-03-01 22:40:17 +0530

    Stories Half done

---
+++ Data/Users.js
@@ -0,0 +1,32 @@
+export const USERS = [
+  {
+    id: "1",
+    user: "Elon_musk",
+    image:
+      "https://en.wikipedia.org/wiki/Elon_Musk#/media/File:Elon_Musk_Royal_Society_(crop2).jpg",
+  },
+  {
+    id: "2",
+    user: "Jeff_Bezos",
+    image:
+      "https://en.wikipedia.org/wiki/Jeff_Bezos#/media/File:Jeff_Bezos_at_Amazon_Spheres_Grand_Opening_in_Seattle_-_2018_(39074799225)_(cropped).jpg",
+  },
+  {
+    id: "3",
+    user: "The_rock",
+    image:
+      "https://en.wikipedia.org/wiki/Dwayne_Johnson#/media/File:Dwayne_Johnson_2014_(cropped).jpg",
+  },
+  {
+    id: "4",
+    user: "Alan_walker",
+    image:
+      "https://en.wikipedia.org/wiki/Alan_Walker_(music_producer)#/media/File:Alan_Walker.jpg",
+  },
+  {
+    id: "5",
+    user: "Will_smith",
+    image:
+      "https://en.wikipedia.org/wiki/Will_Smith#/media/File:TechCrunch_Disrupt_2019_(48834434641)_(cropped).jpg",
+  },
+];
--- components/Home/Stories.js
+++ components/Home/Stories.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
+import { USERS } from "../../Data/Users";
 
 const Stories = () => {
   return (
-    <View>
-      <Text>Stories</Text>
+    <View style={{ marginBottom: 13 }}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {USERS.map((story, index) => (
+          <Image key={story.id} source={{ uri: story.image }} style={styles.story} />
+        ))}
+      </ScrollView>
+      <Text style={{ color: 'white' }}>Stories</Text>
     </View>
-  );
-};
+  )
+}
 
+const styles = StyleSheet.create({
+  story: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+  },
+})
 export default Stories;
