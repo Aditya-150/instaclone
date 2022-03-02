3c53d66abb07bd43a2607fa3e7f2f23703bc8681
Aditya <adityaprasadmohanty150@gmail.com>
2022-03-02 21:37:31 +0530

    Post Footer in process

---
+++ Data/PostFooter.js
@@ -0,0 +1,18 @@
+export const postFooterIcons = [
+  {
+    name: "Like",
+    imageurl: "https://img.icons8.com/windows/32/ffffff/like--v1.png",
+  },
+  {
+    name: "Comment",
+    imageurl: "https://img.icons8.com/metro/50/ffffff/topic.png",
+  },
+  {
+    name: "Share",
+    imageurl: "https://img.icons8.com/external-flatart-icons-outline-flatarticons/64/ffffff/external-send-email-flatart-icons-outline-flatarticons.png",
+  },
+  {
+    name: "Save",
+    imageurl: "https://img.icons8.com/fluency-systems-regular/48/ffffff/bookmark-ribbon--v1.png",
+  },
+];
