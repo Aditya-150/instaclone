c290d6a0c52cc61229fea7faf0dedb9b09111071
Aditya <adityaprasadmohanty150@gmail.com>
2022-03-05 09:21:33 +0530

    Added new folder and file

---
+++ screens/NewPostScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+const NewPostScreen = () => {
+  return (
+    <SafeAreaView>
+      <AddNewPost/>
+    </SafeAreaView>
+  );
+};
+
+export default NewPostScreen;
