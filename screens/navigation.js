5bd1a2ddc7ea13f8b540205510c02ba2edb22ee6
Aditya <adityaprasadmohanty150@gmail.com>
2022-03-05 18:50:09 +0530

    Added navigation.js file.. will work on it later..

---
+++ screens/navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const navigation = () => {
+  return (
+    <View>
+      <Text>navigation</Text>
+    </View>
+  );
+};
+
+export default navigation;
