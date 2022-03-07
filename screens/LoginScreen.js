486638acc620283dda6947d0c3ecb87ab071ea3b
Aditya <adityaprasadmohanty150@gmail.com>
2022-03-07 20:17:49 +0530

    Added insta logo in Login Screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
+const INSTAGRAM_LOGO =
+  "https://img.icons8.com/fluency/512/000000/instagram-new.png";
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={{ uri: INSTAGRAM_LOGO }} style={{width: 100, height: 100}}/>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: 50,
+    paddingHorizontal: 12,
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginTop: 60,
+  }
+});
 
 export default LoginScreen;
